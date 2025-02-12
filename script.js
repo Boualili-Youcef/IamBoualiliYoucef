a9f10bebf207af51589e0f01f0d3ef98d336df24
Boualili Youcef <youcefboualili0@gmail.com>
2025-02-12 18:33:55 +0100

    Refactor menu behavior, add rotating globe feature, and update project links in index.html

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ function opentab(tabname){
 let sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
-    sidemenu.style.right = "0";
+    sidemenu.style.right = "-200px";
 }
 
 function closemenu(){
-    sidemenu.style.right = "-200px";
+    sidemenu.style.right = "-400px";
 }
 
 // Modified toggleLanguage function using a global language flag
@@ -36,27 +36,3 @@ function toggleLanguage() {
     });
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw6zlWjuTtg16ac1JPLBxkGczNTV7MmImU4RDWZ3viRdqRUVQu9Iv87uXM98T-t-cNV/exec';
-const form = document.forms['contact-form'];
-const msg = document.getElementById("msg");
-let formSubmitted = false;
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    if (formSubmitted) return; // Empêcher les soumissions multiples
-    formSubmitted = true;
-
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent successfully";
-            setTimeout(function(){
-                msg.innerHTML = "";
-                form.reset();
-                formSubmitted = false; // Réactiver le formulaire après le délai
-            }, 5000);
-        })
-        .catch(error => {
-            console.error('Error!', error.message);
-            formSubmitted = false; // Réactiver le formulaire en cas d'erreur
-        });
-});
